ab6032923f7d7d8935e2ec267a44b3b8b2b25ab5
Audrey Chenu <97744698+AudreyChenu@users.noreply.github.com>
2022-04-01 17:16:09 +0200

    début d'ajout de l'historique

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
 const fetchDataBtn = document.querySelector('#submit')
-const result = document.querySelector('#content')
+const saveDataBtn = document.querySelector('#save-data')
+const card = document.querySelector('#activity-card')
+const savedData = document.getElementById('#content-2')
+const title = document.querySelector('#activitytitle')
+const type = document.querySelector('#activitytype')
+const pax = document.querySelector('#activityparticipants')
+const price = document.querySelector('#activityprice')
 
+let div = document.querySelector("#content-2")
 
 const getData = function() {
-  result.innerText = 'Loading....'
-  fetch("data.json")
+  title.innerText = 'Loading....'
+  type.innerText = ''
+  pax.innerText = ''
+ price.innerText = ''
+
+  fetch("https://www.boredapi.com/api/activity")
     .then(res => res.json())
     .then( (data) => {
+      console.log(data);
 
-        for (let i in data){
-document.getElementById("content").innerText = JSON.stringify(data[i]);
+      if (data.price == 0){
+
+        data.price = "Yes";
+      }
+
+      else {
+
+        data.price = "No"
+      }
+
+     title.innerHTML = data.activity 
+      type.innerHTML = 'Type : ' + data.type  
+      pax.innerHTML =  'Participants : ' + data.participants
+      price.innerHTML = 'Free?  ' + data.price 
+
+
+console.log(data.length)
+
+      var refTable = document.getElementById('TableA');
+
+  // Insère une ligne dans la table à l'indice de ligne 0
+  var nouvelleLigne = refTable.createElement("tr");
+
+  for (var j = 0; i < 4; i++) {
+  var nouvelleCellule = nouvelleLigne.createElement("td");
+
+  // Ajoute un nœud texte à la cellule
+  var cardText = document.createTextNode(card.innerText)
+
+
+  nouvelleCellule.appendChild(cardText);
 }
+
     })
     .catch(error => console.log(error))
 }
